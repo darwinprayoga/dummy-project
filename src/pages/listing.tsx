4556f6ae3f6d4b998ef22c66430ec94b3e5e73f5
batarazakaria <batarazakaria2@gmail.com>
2023-11-08 22:56:24 +0700

    adding fitur Update/edit in Expense Tracker (listing.tsx)

--- src/pages/listing.tsx
+++ src/pages/listing.tsx
@@ -8,25 +8,40 @@ import {
   onSnapshot,
   deleteDoc,
   doc,
+  updateDoc,
 } from "firebase/firestore";
 import { db } from "../../firebase";
 import { Inter } from "next/font/google";
-import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [items, setItems] = useState<
-    Array<{ name: string; price: string; id: string }>
-  >([]);
+
+  //useState array data dari firestore database
+  const [items, setItems] = useState<Array<{ name: string; price: string; id: string }>>([]);
+
+  //useState yang digunakan untuk menyimpan data item yang akan ditambahkan ke database.
   const [newItem, setNewItem] = useState<{
     name: string;
     price: string;
     id: string;
   }>({ name: "", price: "", id: "" });
+
+
+  const [editItem, setEditItem] = useState<{
+    name: string;
+    price: string;
+    id: string;
+  } | null>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  //useState yang menyimpan variabel total perhitungan
   const [total, setTotal] = useState<number>(0);
 
-  // Add item to database
+  // membuat function untuk listing //
+
+  // 1. Add item to database (create)
   const addItem = async (e: React.FormEvent) => {
     e.preventDefault();
     if (newItem.name !== "" && newItem.price !== "") {
@@ -38,7 +53,7 @@ export default function Home() {
     }
   };
 
-  // Read items from database
+  // 2. Read items from database (read)
   useEffect(() => {
     const q = query(collection(db, "items"));
     const unsubscribe = onSnapshot(q, (snapshot) => {
@@ -56,7 +71,7 @@ export default function Home() {
       });
       setItems(itemsArr);
 
-      // Read total from itemsArr
+      // 2.1 Read total from itemsArr (read)
       const calculateTotal = () => {
         const totalPrice = itemsArr.reduce(
           (sum, item) => sum + parseFloat(item.price),
@@ -69,14 +84,28 @@ export default function Home() {
     });
   }, []);
 
-  // Delete items from database
+  // 3. Delete items from database (delete)
   const deleteItem = async (id: string) => {
     await deleteDoc(doc(db, "items", id));
   };
 
+  // 4. Update Items from database (update/edit)
+  const updateItem = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (editItem) {
+      const itemRef = doc(db, "items", editItem.id);
+      await updateDoc(itemRef, {
+        name: editItem.name.trim(),
+        price: editItem.price,
+      });
+      setEditItem(null); // Setelah berhasil mengupdate, reset editItem ke null
+      setIsEditing(false); // Set isEditing ke false
+    }
+  };
+
   return (
     <main className="bg-white ">
-      <Navbar />
+
       <div className="min-h-screen z-10 w-full max-w-5xl items-center justify-between text-sm ">
         <div className="flex items-center w-screen py-8 px-12 xl:px-24 lg:px-18 md:px-12 md:py-12 sm:px-8 sm:py-8">
           <div className="w-1/2 flex flex-col justify-center space-y-4 xl:space-y-8 xl:py-24 lg:space-y-8 lg:py-24 md:space-y-4 sm:space-y-4">
@@ -96,29 +125,30 @@ export default function Home() {
           <div className="shadow-2xl bg-white rounded-lg items-center justify-between p-6 h-min">
             <form className="grid grid-cols-6 items-center text-black ">
               <input
-                value={newItem.name}
-                onChange={(e) =>
-                  setNewItem({ ...newItem, name: e.target.value })
-                }
+                value={isEditing ? (editItem ? editItem.name : "") : newItem.name}
                 className="col-span-3 p-3 border rounded-lg hover:border-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
-                type="text"
-                placeholder="Enter Item"
+                onChange={(e) =>
+                  isEditing && editItem
+                    ? setEditItem({ ...editItem, name: e.target.value })
+                    : setNewItem({ ...newItem, name: e.target.value })
+                }
               />
               <input
-                value={newItem.price}
-                onChange={(e) =>
-                  setNewItem({ ...newItem, price: e.target.value })
-                }
+              type="number"
+                value={isEditing ? (editItem ? editItem.price : "") : newItem.price}
                 className="col-span-2 p-3 border mx-3 rounded-lg hover:border-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 "
-                type="number"
-                placeholder="Enter $"
+                onChange={(e) =>
+                  isEditing && editItem
+                    ? setEditItem({ ...editItem, price: e.target.value })
+                    : setNewItem({ ...newItem, price: e.target.value })
+                }
               />
               <button
-                onClick={addItem}
+                onClick={isEditing ? updateItem : addItem}
                 className="bg-[#3461FF] text-white  p-3 text-xl rounded-lg"
                 type="submit"
               >
-                +
+                {isEditing ? "Update" : "+"}
               </button>
             </form>
             <ul>
@@ -129,8 +159,17 @@ export default function Home() {
                     <span>${item.price}</span>
                   </div>
                   <button
+                    onClick={() => {
+                      setIsEditing(true);
+                      setEditItem(item);
+                    }}
+                    className="bg-[#808080] ml-3 w-20 rounded-lg"
+                  >
+                    Edit
+                  </button>
+                  <button
                     onClick={() => deleteItem(item.id)}
-                    className="bg-[#ff3d47] ml-3 w-16 rounded-lg"
+                    className="bg-[#ff3d47] ml-3 w-20 rounded-lg"
                   >
                     X
                   </button>
