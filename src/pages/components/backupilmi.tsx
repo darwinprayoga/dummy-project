1d8cbf0208ada5174e976f0433f994bdd4ed09d9
ilmi's <zulilmiihsn@gmail.com>
2023-11-10 21:15:22 +0700

    Push gmeet

---
+++ src/pages/components/backupilmi.tsx
@@ -0,0 +1,16 @@
+<div className="md:hidden">
+      <div>
+        <button><img src="/icon/dropdown-button.png" alt=""/></button>
+      </div>
+      <div className="bg-white w-32 space-y-4 p-4 right-0 absolute block items-end z-10 shadow-xl font-bold rounded-xl">
+        <button className="text-black rounded-xl w-full p-2 xl:py-2 xl:px-3 xl:text-lg lg:text-xs lg:p-2 md:p-1 md:text-xs md:rounded-1 md:mx-0 sm:text-xs">
+          Book A Call
+        </button>
+        <button
+            className="bg-[#3461FF] rounded-xl w-full p-2 xl:py-2 xl:px-3 xl:text-lg lg:text-xs lg:p-2 md:p-1 md:text-xs md:rounded-1 md:mx-0 sm:text-xs"
+            onClick={googleSignIn}
+          >
+            Login
+        </button>
+      </div>
+      </div>
